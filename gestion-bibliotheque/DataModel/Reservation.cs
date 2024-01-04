5806b5e89b4b8e8443eff0261a6cb03240980525
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2024-01-04 17:01:32 +0100

    fix reservations liste errors

---
+++ gestion-bibliotheque/DataModel/Reservation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion_bibliotheque.DataModel
+{
+   public class Reservation
+    {
+        public int ReservationID { get; set; }
+
+        public int AdherentID { get; set; }
+
+        public int LivreID { get; set; }
+
+        public DateTime DateReservation { get; set; }
+
+        public DateTime? DateRetour { get; set; }
+
+        public bool Prolonge { get; set; }
+        public Livre Livre { get; set; }
+        public Adherent Adherent { get; set; }
+        public decimal MontantAmende { get; set; }
+
+
+        public string LivreTitre
+        {
+            get { return Livre.Titre; }
+        }
+
+        public string AdherntName
+        {
+            get { return Adherent.Nom; }
+        }
+
+        public String ProlongState
+        {
+            get { return Prolonge ? "en Cour" : "terminée";}
+        }
+
+    }
+}
--- gestion-bibliotheque/View/Reservation.xaml.cs
+++ gestion-bibliotheque/View/Reservation.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using gestion_bibliotheque.DataModel;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,28 @@ namespace gestion_bibliotheque.View
     /// </summary>
     public partial class Reservation : UserControl
     {
+        ReservationDbHelper reservationDbHelper = new ReservationDbHelper();
+        private ObservableCollection<DataModel.Reservation> resevationCollection;
         public Reservation()
         {
             InitializeComponent();
+            LoadData();
         }
+
+
+        public void LoadData()
+        {
+            List<DataModel.Reservation> reservations = reservationDbHelper.GetReservations();
+            resevationCollection = new ObservableCollection<DataModel.Reservation>(reservations);
+            ReservationsDataGrid.ItemsSource = reservations;
+
+        }
+
+        public void membersDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e) { }
+        public void editeReservation(object sender, RoutedEventArgs e) { }
+        public void Delete(object sender, RoutedEventArgs e) { }
+
+
+
     }
 }
